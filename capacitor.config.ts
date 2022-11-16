bd3deed54ac2079eb4f1cff82424142c87a67e8c
Aero48 <56200404+Aero48@users.noreply.github.com>
2022-11-16 16:14:14 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'favoriteModList',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
